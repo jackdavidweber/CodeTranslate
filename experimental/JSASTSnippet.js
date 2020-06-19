0055459c56579bf3c5ebd346ef0a661086f8a053
Steph Walsh <srwalsh21@gmail.com>
2020-06-19 10:59:03 -0400

    Add support for basic var assignments

--- experimental/JSASTSnippet.js
+++ experimental/JSASTSnippet.js
@@ -3,33 +3,79 @@
 
 //const parser = require('@babel/parser').parse;
 import babel from '@babel/core';
-const inputCode = 'console.log("hello world")';
-let outputCode = "";
+//const inputCode = 'console.log("hello world")\n console.log(2)\n console.log("helloo", "world")';
+const inputCode = 'let x  = "sa"\n const y = 2;\n console.log("hello world")';
 
 // parse the code -> ast
 const ast = babel.parse(inputCode)
 
+let genericAst = {
+	"type": "root",
+	"body": []
+}
+
+
 // Approach using traverse for console.log() -> print()
 babel.traverse(ast, {
   CallExpression: function(path) { 
-    const args = (path.node.arguments);
-
     // function called on object
     if (path.node.callee.type == "MemberExpression") {
       const object = path.node.callee.object.name;
       const func = path.node.callee.property.name;
-      // TODO: check if object.func is in map (console.log)
-      outputCode += 'print("'
-      // TODO: seperate functions to parse args 
-      outputCode += args[0].value;
-      outputCode += '")\n';
+
+      if (object == "console" && func == "log") {
+        let args = [];
+        for (const arg of path.node.arguments) {
+          // handles printing variables
+          if (arg.type == "Identifier") {
+            args.push(arg.name);
+          } else {
+            args.push(arg.extra.raw);
+          }
+        }
+        genericAst.body.push({"type": "logStatement", "args" : args});
+      }
     }
+    
     // function called
     if (path.node.callee.type == "Identifer") {
       const funcName = path.node.callee.name; 
     }
+  },
+
+  // handles var declerations
+  // current supports only numbers and strings 
+  VariableDeclaration(path) {
+    // TODO: handle multiple declarations
+    const name = path.node.declarations[0].id.name;
+    const val = path.node.declarations[0].init.extra.raw;
+    genericAst.body.push({"type": "varAssign", "varId" : name, "varValue" : val});
   }
+
 });
+
+console.log(genericAst);
+
+// read from generic AST
+for (const statement of genericAst.body) {
+  // translate logStatement to Python
+  if (statement.type == "logStatement") {
+    let translation = "print(";
+    for (const arg of statement.args) {
+      // NOTE: console.log("Hello", "world") -> print("Hello""world")
+      // TODO: fix when multiple args given
+      translation += arg;
+    }
+    translation += ")";
+    console.log(translation);
+  }
+
+  // varr declaratioon
+  if (statement.type == "varAssign") {
+    console.log(statement.varId += " = " + statement.varValue);
+  }
+}
+
 
 // loop through all expressions in body 
 //ast.program.body.forEach(handleExpression);
@@ -57,4 +103,3 @@ function handleExpression(expression) {
     }
   }
 }
-console.log("translated: " + outputCode);
