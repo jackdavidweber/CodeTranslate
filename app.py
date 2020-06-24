204063ab363c077deed408396a7efe839e831448
Jack <jackdavidweber@gmail.com>
2020-06-24 12:23:42 -0700

    added basic hello name post request

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
 
 
@@ -7,9 +7,17 @@ app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
+parser = reqparse.RequestParser()
+parser.add_argument('name')
+
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+    
+    def post(self):
+        args = parser.parse_args()
+        print(args['name'])
+        return {'hello': args['name']}
 
 api.add_resource(HelloWorld, '/')
 
