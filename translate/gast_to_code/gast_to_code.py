25d19eb127133200b029e5a8f9fc8f1f3a8627d9
Jack <jackdavidweber@gmail.com>
2020-06-24 15:50:40 -0700

    get both js and python working in same gast_to_code file

--- translate/gast_to_code/gast_to_code.py
+++ translate/gast_to_code/gast_to_code.py
@@ -1,15 +1,15 @@
 
-# helpers
+# general_helpers
 def value_to_str(val):
     if type(val) == str:
         return '"' + val + '"'
     else:
         return str(val)
 
-def body(gast_list):
+def body(gast_list, out_lang):
     out = ""
     for gast in gast_list:
-        out += gast_router(gast)
+        out += gast_router(gast, out_lang)
         out += "\n"
 
     return out[:-1] # remove \nS
@@ -21,7 +21,16 @@ def list_to_csv_str(l):
     
     return s[:-2] # remove last comma and space
 
-# mains
+# py_specific_helpers
+def py_logStatement(gast):
+    arg_string = list_to_csv_str(gast["args"])
+    return "print(" + arg_string + ")"
+
+def py_varAssign(gast):
+    value = value_to_str(gast["varValue"])
+    return gast["varId"] + " = " + value
+
+# js_specific_helpers
 def js_logStatement(gast):
     arg_string = list_to_csv_str(gast["args"])
     return "console.log(" + arg_string + ")"
@@ -32,26 +41,37 @@ def js_varAssign(gast):
 
 out = {
     "logStatement": {
-        "py": "py_logStatement",
+        "py": py_logStatement,
         "js": js_logStatement
     },
     "varAssign": {
-        "py": "py_varAssign",
+        "py": py_varAssign,
         "js": js_varAssign,
     }
 }
 
-def gast_router(gast):
+"""
+gast router that takes generic ast and the output language
+that the gast needs to be converted to and executes the
+conversion recursively
+out_lang correspond to the language codes defined in datastructure:
+javascript: js
+python: py
+"""
+def gast_router(gast, out_lang):
     if gast["type"] == "root":
-        return body(gast["body"])
+        return body(gast["body"], out_lang)
 
     elif gast["type"] == "logStatement":
-        return out["logStatement"]["js"](gast)
+        return out["logStatement"][out_lang](gast)
 
     elif gast["type"] == "varAssign":
-        return varAssign(gast)
-    
-gast =  {
+        return out["varAssign"][out_lang](gast)
+
+
+# EXAMPLE
+
+example_gast =  {
             "type": "root",
             "body": [
                 {
@@ -66,5 +86,18 @@ gast =  {
             ]   
         }
 
+#EXAMPLE Python
+print(gast_router(example_gast, "py"))
+"""
+output:
+    print(hello world, hi, 3)
+    x = 5
+"""
 
-print(gast_router(gast))
+#EXAMPLE javascript
+print(gast_router(example_gast, "js"))
+"""
+output:
+    console.log(hello world, hi, 3)
+    const x = 5
+"""
