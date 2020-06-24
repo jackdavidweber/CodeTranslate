2c4e0ff33d99896bffd53eebe6dc17573dc72667
Jack <jackdavidweber@gmail.com>
2020-06-24 16:54:04 -0700

    cleaned so that backend would still work for frontend before it is updated

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ api = Api(app)
 CORS(app)
 
 parser = reqparse.RequestParser()
-parser.add_argument('in_code')
+parser.add_argument('input')
 parser.add_argument('in_lang')
 parser.add_argument('out_lang')
 
@@ -31,9 +31,14 @@ class Translate(Resource):
     def post(self):
         # bring in post arguments
         args = parser.parse_args()
-        input_code = args['in_code']
+        input_code = args['input']
         input_lang = args['in_lang']
         output_lang = args['out_lang']
+
+        # TODO: remove this once frontend is updated
+        if input_lang == None and output_lang == None:
+            input_lang = "js"
+            output_lang = "py"
 
         print(input_code, input_lang, output_lang)
         if input_lang == "js":
