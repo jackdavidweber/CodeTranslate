6ed7d5566d576cd38df5bf5385a0afd2c173412a
Stephanie Walsh <srwalsh21@gmail.com>
2020-06-24 15:20:56 -0400

    Create python AST from string instead of file

--- translate/py_main.py
+++ translate/py_main.py
@@ -13,11 +13,11 @@ import py_router
 takes pyton file and converts it to a node
 node is then dealt with by node_to_gast 
 """
-def py_to_gast(python_input_filename):
-    input_ast =  astor.code_to_ast.parse_file(python_input_filename)
+def py_to_gast(python_input):
+    input_ast = ast.parse(python_input)
     print(astor.dump_tree(input_ast))
     return py_router.node_to_gast(input_ast)
 
-fileName = "py_sample_code.py"
-gast = py_to_gast(fileName)
+file = "print('hello world')"
+gast = py_to_gast(file)
 print(gast)
