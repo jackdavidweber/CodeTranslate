2abdb7bba85289bf7ace0fce69d6e41a3de3d6bb
Cory O'Shea <cjoshea9@gmail.com>
2020-06-25 15:24:51 -0600

    Update js router with how arrays are handled and added test cases for arrays and booleans

--- test_var_assign.py
+++ test_var_assign.py
@@ -2,20 +2,21 @@ import unittest2
 import main
 
 class TestLogStatement(unittest2.TestCase):
-    def test_string_assign_js_to_py(self):
+    def test_string_assign(self):
         self.assertEqual('x = "hi"', main.main('let x = "hi"', 'js', 'py'))
-
-    def test_string_assign_py_to_js(self):
         self.assertEqual('const test = "working"', main.main('test = "working"', 'py', 'js'))
     
-    def test_int_assign_js_to_py(self):
+    def test_int_assign(self):
         self.assertEqual('num = 109', main.main('const num = 109', 'js', 'py'))
-    
-    def test_int_assign_py_to_js(self):
         self.assertEqual('const n = 12', main.main('n = 12', 'py', 'js'))
     
-    def test_array_assign_js_to_py(self):
+    def test_array_assign(self):
         self.assertEqual('arr = [3, 6]', main.main('let arr = [3, 6]', 'js', 'py'))
+        self.assertEqual('const nest = [[1, 9], [2, 8]]', main.main('nest = [[1,9],[2,8]]', 'py', 'js'))
+    
+    def test_boolean_assign(self):
+        self.assertEqual('boo = True', main.main('const boo=True', 'js', 'py'))
+        self.assertEqual('const lean = false', main.main('lean = False', 'py', 'js'))
 
 if __name__ == '__main__':
     unittest2.main()
