f46cd35b35d753529f0436288168eb623e5b2e02
Jack <jackdavidweber@gmail.com>
2020-06-30 09:03:22 -0700

    add if elif else test as per michael's rec

--- test_conditionals.py
+++ test_conditionals.py
@@ -20,5 +20,11 @@ class TestConditionals(unittest2.TestCase):
         self.assertEqual(py_code, main.main(js_code, 'js', 'py'))
         self.assertEqual(js_code, main.main(py_code, 'py', 'js'))
 
+    def test_elif_else(self):
+        js_code = 'if (1) {\n\tconsole.log("1 is true")\n} else if (2) {\n\tconsole.log("2 is true")\n} else {\n\tconsole.log("nothing is true")\n}'
+        py_code = 'if (1):\n\tprint("1 is true")\nelif (2):\n\tprint("2 is true")\nelse:\n\tprint("nothing is true")'
+        self.assertEqual(py_code, main.main(js_code, 'js', 'py'))
+        self.assertEqual(js_code, main.main(py_code, 'py', 'js'))
+
 if __name__ == '__main__':
     unittest2.main()
