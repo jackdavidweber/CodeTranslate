b05da210c7478bdd968b2c92e072214ac856c1c5
Cory O'Shea <cjoshea9@gmail.com>
2020-07-01 08:13:23 -0600

    Combined 6 tests into 3 for conditionals so both languages in each case

--- test/test_code_to_gast_conditionals.py
+++ test/test_code_to_gast_conditionals.py
@@ -14,7 +14,8 @@ import js_main
 
 class test_code_to_gast_conditionals(unittest2.TestCase):
     maxDiff = None
-    def test_ifs_js(self):
+    def test_ifs(self):
+        py_code = 'if (True):\n\tprint("This is true")' 
         js_code = 'if (true) {\n\tconsole.log("This is true")\n}'
         expected_gast = {
             'type': 'root',
@@ -29,9 +30,11 @@ class test_code_to_gast_conditionals(unittest2.TestCase):
                 'test': {'type': 'bool', 'value': 1}
                 }]
             }
+        self.assertEqual(expected_gast, py_main.py_to_gast(py_code))
         self.assertEqual(expected_gast, js_main.js_to_gast(js_code))
 
-    def test_else_js(self):
+    def test_else(self):
+        py_code = 'if (1):\n\tprint("1 is true")\nelse:\n\tprint("1 is NOT true")\n'
         js_code = 'if (1) {\n\tconsole.log("1 is true")\n} else {\n\tconsole.log("1 is NOT true")\n}' # TODO: consider adding ; after console.log()
         expected_gast = {
             'type': 'root', 
@@ -50,9 +53,11 @@ class test_code_to_gast_conditionals(unittest2.TestCase):
                 'test': {'type': 'num', 'value': 1}
                 }]
             }
+        self.assertEqual(expected_gast, py_main.py_to_gast(py_code))
         self.assertEqual(expected_gast, js_main.js_to_gast(js_code))
    
-    def test_elif_js(self):
+    def test_elif(self):
+        py_code = 'if (1):\n\tprint("1 is true")\nelif (2):\n\tprint("2 is true")\n\tprint("second line")\n'
         js_code = 'if (1) {\n\tconsole.log("1 is true")\n} else if (2) {\n\tconsole.log("2 is true")\n\tconsole.log("second line")\n}'
         expected_gast = {
             'type': 'root', 
@@ -83,78 +88,9 @@ class test_code_to_gast_conditionals(unittest2.TestCase):
                 'test': {'type': 'num', 'value': 1}
                 }]
             }     
+        self.assertEqual(expected_gast, py_main.py_to_gast(py_code))
         self.assertEqual(expected_gast, js_main.js_to_gast(js_code))
 
-    def test_ifs_py(self):
-        py_code = 'if (True):\n\tprint("This is true")'   
-        expected_gast = {
-            'type': 'root',
-            'body': [{
-                'type': 'if',
-                 'body': [{
-                     'type': 'funcCall',
-                     'value': {'type': 'logStatement'},
-                     'args': [{'type': 'str', 'value': 'This is true'}]
-                    }],
-                'orelse': [],
-                'test': {'type': 'bool', 'value': 1}
-                }]
-            }
-        self.assertEqual(expected_gast, py_main.py_to_gast(py_code))
-
-    def test_else_py(self):
-        py_code = 'if (1):\n\tprint("1 is true")\nelse:\n\tprint("1 is NOT true")\n'
-        expected_gast = {
-            'type': 'root', 
-            'body': [{
-                'type': 'if',
-                'body': [{
-                     'type': 'funcCall',
-                     'value': {'type': 'logStatement'}, 
-                     'args': [{'type': 'str', 'value': '1 is true'}]
-                     }], 
-                'orelse': [{
-                         'type': 'funcCall',
-                         'value': {'type': 'logStatement'},
-                         'args': [{'type': 'str', 'value': '1 is NOT true'}]
-                         }], 
-                'test': {'type': 'num', 'value': 1}
-                }]
-            }
-        self.assertEqual(expected_gast, py_main.py_to_gast(py_code))
-   
-    def test_elif_py(self):
-        py_code = 'if (1):\n\tprint("1 is true")\nelif (2):\n\tprint("2 is true")\n\tprint("second line")\n'
-        expected_gast = {
-            'type': 'root', 
-            'body': [{
-                'type': 'if', 
-                'body': [{
-                    'type': 'funcCall',
-                    'value': {'type': 'logStatement'}, 
-                    'args': [{'type': 'str', 'value': '1 is true'}]
-                    }], 
-                'orelse': [{
-                    'type': 'if', 
-                    'body': [
-                        {
-                        'type': 'funcCall',
-                        'value': {'type': 'logStatement'}, 
-                        'args': [{'type': 'str', 'value': '2 is true'}]
-                        },
-                        {
-                        'type': 'funcCall',
-                        'value': {'type': 'logStatement'}, 
-                        'args': [{'type': 'str', 'value': 'second line'}]
-                        }
-                    ], 
-                    'orelse': [], 
-                    'test': {'type': 'num', 'value': 2}
-                    }], 
-                'test': {'type': 'num', 'value': 1}
-                }]
-            }     
-        self.assertEqual(expected_gast, py_main.py_to_gast(py_code))
 
 if __name__ == '__main__':
     unittest2.main()
