a42ae54bc7867b86a25d50fda9416de050eff005
Jack <jackdavidweber@gmail.com>
2020-07-06 08:09:42 -0700

    removed all global variables. combined py and js specific tests

--- test/test_gast_to_code.py
+++ test/test_gast_to_code.py
@@ -6,127 +6,6 @@ import unittest2
 import gast_to_code.gast_to_code_main as gtc
 
 
-gast_arr = {
-	"type": "arr",
-	"elts": 
-		[
-            {
-                "type": "str",
-                "value": "hello"
-            },
-            {
-                "type": "arr",
-                "elts":
-                    [
-                        {
-                            "type": "num",
-                            "value": 1
-                        },
-                        {
-                            "type": "num",
-                            "value": 2
-                        }
-                    ]
-            }
-		]
-}
-
-gast_binOp_add = {'type': 'binOp', 'op': '+', 'left': {'type': 'num', 'value': 3}, 'right': {'type': 'num', 'value': 4}}
-
-gast_binOp_bitwise = {'type': 'binOp', 'op': '&', 'left': {'type': 'num', 'value': 1}, 'right': {'type': 'num', 'value': 3}}
-
-gast_binOp_add_sub_mult_div = {'type': 'binOp', 'op': '-', 'left': {'type': 'binOp', 'op': '+', 'left': {'type': 'num', 'value': 1}, 'right': {'type': 'num', 'value': 2}}, 'right': {'type': 'binOp', 'op': '/', 'left': {'type': 'binOp', 'op': '*', 'left': {'type': 'num', 'value': 3}, 'right': {'type': 'num', 'value': 4}}, 'right': {'type': 'num', 'value': 5}}}
-
-gast_boolOp_and = {'type': 'boolOp', 'op': '&&', 'left': {'type': 'bool', 'value': 1}, 'right': {'type': 'bool', 'value': 0}}
-
-gast_boolOp_or_and = {'type': 'boolOp', 'op': '||', 'left': {'type': 'bool', 'value': 1}, 'right': {'type': 'boolOp', 'op': '&&', 'left': {'type': 'bool', 'value': 0}, 'right': {'type': 'num', 'value': 4}}}
-
-gast_logStatement_bool = {
-            "type": "root",
-            "body": [
-                {
-                    "type": "funcCall",
-                    "args": [
-                        {
-                            "type": "bool",
-                            "value": 0
-                        }
-                    ],
-                    "value": {
-                        "type": "logStatement"
-                    }
-                }
-            ]
-        }
-
-gast_logStatement = {
-            "type": "root",
-            "body": [
-                {
-                    "type": "funcCall",
-                    "args": [
-                        {
-                            "type": "str",
-                            "value": "hello world"
-                        },
-                        {
-                            "type": "num",
-                            "value": 5
-                        }
-                    ],
-                    "value": {
-                        "type": "logStatement"
-                    }
-                }
-            ]
-        }
-
-gast_varAssign_let = {
-	"type": "root",
-	"body": [
-		{
-			"type": "varAssign",
-			"kind": "let",
-			"varId": {
-                "type": "name",
-                "value": "x"
-            },
-            "varValue": 
-                {
-                    "type": "num",
-                    "value": 5
-                }
-        },
-		]
-}
-
-gast_varAssign_const = {
-	"type": "root",
-	"body": [
-		{
-			"type": "varAssign",
-			"kind": "const",
-			"varId": {
-                "type": "name",
-                "value": "x"
-            },
-            "varValue": 
-                {
-                    "type": "num",
-                    "value": 5
-                }
-        },
-		]
-}
-
-gast_multi_body = {
-    "type": "root",
-    "body": [
-        { "type": "varAssign", "kind": "const", "varId": {"type": "name", "value": "x"}, "varValue": { "type": "num", "value": 5 } },
-        { "type": "varAssign", "kind": "const", "varId": {"type": "name", "value": "x"}, "varValue": { "type": "num", "value": 5 } },
-    ]
-}
-
 class TestGastToCode(unittest2.TestCase):
     # test primitives
     def test_primitive_str(self):
@@ -153,61 +32,173 @@ class TestGastToCode(unittest2.TestCase):
         self.assertEqual('false', gtc.gast_to_code(gast_false, "js"))
         self.assertEqual('False', gtc.gast_to_code(gast_false, "py"))
 
-
     # test other types
     def test_nested_arr(self):
+        gast_arr = {
+            "type": "arr",
+            "elts": 
+                [
+                    {
+                        "type": "str",
+                        "value": "hello"
+                    },
+                    {
+                        "type": "arr",
+                        "elts":
+                            [
+                                {
+                                    "type": "num",
+                                    "value": 1
+                                },
+                                {
+                                    "type": "num",
+                                    "value": 2
+                                }
+                            ]
+                    }
+                ]
+        }
+
         self.assertEqual('["hello", [1, 2]]', gtc.gast_to_code(gast_arr, "py"))
         self.assertEqual('["hello", [1, 2]]', gtc.gast_to_code(gast_arr, "js"))
 
     def test_binOp_add (self):
+        gast_binOp_add = {'type': 'binOp', 'op': '+', 'left': {'type': 'num', 'value': 3}, 'right': {'type': 'num', 'value': 4}}
+
         self.assertEqual('3 + 4', gtc.gast_to_code(gast_binOp_add, "py"))
         self.assertEqual('3 + 4', gtc.gast_to_code(gast_binOp_add, "js"))
 
     def test_binOp_bitwise (self):
+        gast_binOp_bitwise = {'type': 'binOp', 'op': '&', 'left': {'type': 'num', 'value': 1}, 'right': {'type': 'num', 'value': 3}}
+
         self.assertEqual('1 & 3', gtc.gast_to_code(gast_binOp_bitwise, "py"))
         self.assertEqual('1 & 3', gtc.gast_to_code(gast_binOp_bitwise, "js"))
 
     def test_binOp_add_sub_mult_div (self):
+        gast_binOp_add_sub_mult_div = {'type': 'binOp', 'op': '-', 'left': {'type': 'binOp', 'op': '+', 'left': {'type': 'num', 'value': 1}, 'right': {'type': 'num', 'value': 2}}, 'right': {'type': 'binOp', 'op': '/', 'left': {'type': 'binOp', 'op': '*', 'left': {'type': 'num', 'value': 3}, 'right': {'type': 'num', 'value': 4}}, 'right': {'type': 'num', 'value': 5}}}
+
         self.assertEqual('1 + 2 - 3 * 4 / 5', gtc.gast_to_code(gast_binOp_add_sub_mult_div, "py"))
         self.assertEqual('1 + 2 - 3 * 4 / 5', gtc.gast_to_code(gast_binOp_add_sub_mult_div, "js"))
 
-    def test_boolOp_and_py (self):
-        self.assertEqual('True and False', gtc.gast_to_code(gast_boolOp_and, "py"))
+    def test_boolOp_and (self):
+        gast_boolOp_and = {'type': 'boolOp', 'op': '&&', 'left': {'type': 'bool', 'value': 1}, 'right': {'type': 'bool', 'value': 0}}
 
-    def test_boolOp_and_js (self):
+        self.assertEqual('True and False', gtc.gast_to_code(gast_boolOp_and, "py"))
         self.assertEqual('true && false', gtc.gast_to_code(gast_boolOp_and, "js"))
 
-    def test_boolOp_or_and_py (self):
-        self.assertEqual('True or False and 4', gtc.gast_to_code(gast_boolOp_or_and, "py"))
+    def test_boolOp_or_and (self):
+        gast_boolOp_or_and = {'type': 'boolOp', 'op': '||', 'left': {'type': 'bool', 'value': 1}, 'right': {'type': 'boolOp', 'op': '&&', 'left': {'type': 'bool', 'value': 0}, 'right': {'type': 'num', 'value': 4}}}
 
-    def test_boolOp_or_and_js (self):
+        self.assertEqual('True or False and 4', gtc.gast_to_code(gast_boolOp_or_and, "py"))
         self.assertEqual('true || false && 4', gtc.gast_to_code(gast_boolOp_or_and, "js"))
 
     # test logStatement
-    def test_js_logStatement_bool (self):
+    def test_logStatement_bool (self):
+        gast_logStatement_bool = {
+            "type": "root",
+            "body": [
+                {
+                    "type": "funcCall",
+                    "args": [
+                        {
+                            "type": "bool",
+                            "value": 0
+                        }
+                    ],
+                    "value": {
+                        "type": "logStatement"
+                    }
+                }
+            ]
+        }
+
+        self.assertEqual('print(False)', gtc.gast_to_code(gast_logStatement_bool, "py"))
         self.assertEqual('console.log(false)', gtc.gast_to_code(gast_logStatement_bool, "js"))
 
-    def test_js_logStatement_two_arguments(self):
-        self.assertEqual('console.log("hello world", 5)', gtc.gast_to_code(gast_logStatement, "js"))
 
-    def test_py_logStatement_two_arguments(self):
+    def test_js_logStatement_two_arguments(self):
+        gast_logStatement = {
+            "type": "root",
+            "body": [
+                {
+                    "type": "funcCall",
+                    "args": [
+                        {
+                            "type": "str",
+                            "value": "hello world"
+                        },
+                        {
+                            "type": "num",
+                            "value": 5
+                        }
+                    ],
+                    "value": {
+                        "type": "logStatement"
+                    }
+                }
+            ]
+        }
+
         self.assertEqual('print("hello world", 5)', gtc.gast_to_code(gast_logStatement, "py"))
+        self.assertEqual('console.log("hello world", 5)', gtc.gast_to_code(gast_logStatement, "js"))
 
     # test varAssign
     def test_js_varAssign_let(self):
-        self.assertEqual('let x = 5', gtc.gast_to_code(gast_varAssign_let, "js"))
-   
-    def test_py_varAssign_let (self):
+        gast_varAssign_let = {
+            "type": "root",
+            "body": [
+                {
+                    "type": "varAssign",
+                    "kind": "let",
+                    "varId": {
+                        "type": "name",
+                        "value": "x"
+                    },
+                    "varValue": 
+                        {
+                            "type": "num",
+                            "value": 5
+                        }
+                },
+                ]
+        }
+
         self.assertEqual('x = 5', gtc.gast_to_code(gast_varAssign_let, "py"))
+        self.assertEqual('let x = 5', gtc.gast_to_code(gast_varAssign_let, "js"))
 
     def test_js_varAssign_const(self):
-        self.assertEqual('const x = 5', gtc.gast_to_code(gast_varAssign_const, "js"))
-   
-    def test_py_varAssign_const (self):
+        gast_varAssign_const = {
+            "type": "root",
+            "body": [
+                {
+                    "type": "varAssign",
+                    "kind": "const",
+                    "varId": {
+                        "type": "name",
+                        "value": "x"
+                    },
+                    "varValue": 
+                        {
+                            "type": "num",
+                            "value": 5
+                        }
+                },
+                ]
+        }
+
         self.assertEqual('x = 5', gtc.gast_to_code(gast_varAssign_const, "py"))
+        self.assertEqual('const x = 5', gtc.gast_to_code(gast_varAssign_const, "js"))
 
     # test multiple items in body
     def test_multi_body (self):
+        gast_multi_body = {
+            "type": "root",
+            "body": [
+                { "type": "varAssign", "kind": "const", "varId": {"type": "name", "value": "x"}, "varValue": { "type": "num", "value": 5 } },
+                { "type": "varAssign", "kind": "const", "varId": {"type": "name", "value": "x"}, "varValue": { "type": "num", "value": 5 } },
+            ]
+        }
+
         self.assertEqual('x = 5\nx = 5', gtc.gast_to_code(gast_multi_body, "py"))
 
     # TODO: add elif and else if tests once new way of doing logstatements are merged
