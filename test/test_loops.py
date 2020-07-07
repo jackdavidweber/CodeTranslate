e3bb74dc443acf7761ee62389636f0c81e07e1a8
Jack <jackdavidweber@gmail.com>
2020-07-07 11:14:28 -0700

    added range for loop test cases

--- test/test_loops.py
+++ test/test_loops.py
@@ -17,6 +17,30 @@ class TestLoops(unittest2.TestCase):
         py_code = 'while (x < 10):\n\t5'
         self.assertEqual(py_code, main.main(js_code, 'js', 'py'))
         self.assertEqual(js_code, main.main(py_code, 'py', 'js'))
+        
+    def test_for_range_increment_two(self):
+        js_code = 'for (let i = 0; i < 10; i += 2) {\n\t5\n}'
+        py_code = 'for i in range (0,10,2):\n\t5'
+        self.assertEqual(py_code, main.main(js_code, 'js', 'py'))
+        self.assertEqual(js_code, main.main(py_code, 'py', 'js'))
+
+    def test_for_inclusiverange_increment_two(self):
+        js_code = 'for (let i = 0; i <= 10; i += 2) {\n\t5\n}'
+        py_code = 'for i in range (0,12,2):\n\t5'
+        self.assertEqual(py_code, main.main(js_code, 'js', 'py'))
+        self.assertEqual(js_code, main.main(py_code, 'py', 'js'))
+
+    def test_for_range_increment_one(self):
+        js_code = 'for (let i = 0; i < 10; i += 1) {\n\t5\n}'
+        py_code = 'for i in range (0,10):\n\t5'
+        self.assertEqual(py_code, main.main(js_code, 'js', 'py'))
+        self.assertEqual(js_code, main.main(py_code, 'py', 'js'))
+
+    def test_for_range_increment_negative(self):
+        js_code = 'for (let i = 10; i > 0; i -= 1) {\n\t5\n}'
+        py_code = 'for i in range (10,0,-1):\n\t5'
+        self.assertEqual(py_code, main.main(js_code, 'js', 'py'))
+        self.assertEqual(js_code, main.main(py_code, 'py', 'js'))
 
 if __name__ == '__main__':
     unittest2.main()
