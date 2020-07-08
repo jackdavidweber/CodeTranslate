77f23d8a7b48d94cb6ade53979fec8067b8cd40b
Cory O'Shea <cjoshea9@gmail.com>
2020-07-08 09:37:40 -0700

    Gast to code for enum and add comments

--- py_built_in_functions.py
+++ py_built_in_functions.py
@@ -1,5 +1,9 @@
 import enum
 
+"""
+All Python built in functions we support mapped to their corresponding 
+generic AST string
+"""
 class py_built_in_functions(enum.Enum):
     append = "appendStatement"
     pop = "popStatement"
