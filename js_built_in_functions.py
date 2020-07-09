4f84f95d8e9242924b27aaaeab4bf2a0f5b54e12
Cory O'Shea <cjoshea9@gmail.com>
2020-07-09 16:27:51 -0700

    Add string manipulation functions and tests

--- js_built_in_functions.py
+++ js_built_in_functions.py
@@ -5,7 +5,16 @@ All JavaScript built in functions we support mapped to their corresponding
 generic AST string
 """
 class js_built_in_functions(enum.Enum):
+    #Array manipulation
     push = "appendStatement"
     pop = "popStatement"
     sort = "sortStatement"
     concat = "extendStatement"
+    reverse = "reverseStatement"
+
+    #String manipulation
+    search = "findStatement"
+    split = "splitStatement"
+    toUpperCase = "upperStatement"
+    toLowerCase = "lowerStatement"
+    indexOf = "indexStatement"
--- test/test_built_in_functions.py
+++ test/test_built_in_functions.py
@@ -29,6 +29,30 @@ class TestBuiltInFunctions(unittest2.TestCase):
         self.assertEqual('arr1.extend(arr2)', main.main('arr1.concat(arr2)', 'js', 'py'))
         self.assertEqual('arr1.concat(arr2)', main.main('arr1.extend(arr2)', 'py', 'js'))
     
+    def test_reverse(self):
+        self.assertEqual('arr1.reverse()', main.main('arr1.reverse()', 'js', 'py'))
+        self.assertEqual('arr1.reverse()', main.main('arr1.reverse()', 'py', 'js'))
+    
+    def test_string_search(self):
+        self.assertEqual('str.find()', main.main('str.search()', 'js', 'py'))
+        self.assertEqual('str.search()', main.main('str.find()', 'py', 'js'))
+    
+    def test_string_split(self):
+        self.assertEqual('str.split()', main.main('str.split()', 'js', 'py'))
+        self.assertEqual('str.split()', main.main('str.split()', 'py', 'js'))
+    
+    def test_string_lower(self):
+        self.assertEqual('str.lower()', main.main('str.toLowerCase()', 'js', 'py'))
+        self.assertEqual('str.toLowerCase()', main.main('str.lower()', 'py', 'js'))
+    
+    def test_string_upper(self):
+        self.assertEqual('str.upper()', main.main('str.toUpperCase()', 'js', 'py'))
+        self.assertEqual('str.toUpperCase()', main.main('str.upper()', 'py', 'js'))
+    
+    def test_string_index(self):
+        self.assertEqual('str.index()', main.main('str.indexOf()', 'js', 'py'))
+        self.assertEqual('str.indexOf()', main.main('str.index()', 'py', 'js'))
+    
 
 if __name__ == '__main__':
     unittest2.main()
