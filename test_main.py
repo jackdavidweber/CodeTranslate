330016ac8358e389a2c452e25f0853b332756898
Cory O'Shea <cjoshea9@gmail.com>
2020-07-10 11:07:18 -0700

    Fix bug with firebase

--- test_main.py
+++ test_main.py
@@ -1,8 +1,8 @@
 import unittest2
 
 loader = unittest2.TestLoader()
-start_dir = './test'
-suite = loader.discover(start_dir)
+directory = './test'
+suite = loader.discover(directory)
 
 runner = unittest2.TextTestRunner()
 runner.run(suite)
