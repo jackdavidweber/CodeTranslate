f86457be7d5634c3da314b995ac16c6e1748dbb0
Steph Walsh <srwalsh21@gmail.com>
2020-07-10 09:32:37 -0400

    add import to data collect file

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ sys.path.append('code_to_gast/routers')
 import js_main
 import py_main
 import gast_to_code.gast_to_code_main as gtc
-#import data_collection
+import data_collection
 
 """
 input_code: string representing input code
@@ -46,7 +46,7 @@ def main(input_code, input_lang, output_lang):
     else:
         output_code = gtc.gast_to_code(gast, output_lang)
     
-    #data_collection.store_query(input_code, output_code, input_lang, output_lang)
+    data_collection.store_query(input_code, output_code, input_lang, output_lang)
 
     return output_code
     
