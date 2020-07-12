6375b6313e7464c5981b8e9693373afde108fcf3
Jack <jackdavidweber@gmail.com>
2020-07-11 17:22:52 -0700

    added for of tests for multi-lvl indentation

--- test/test_gast_to_code_indentation.py
+++ test/test_gast_to_code_indentation.py
@@ -53,7 +53,14 @@ class TestGastToCodeIndentation(unittest2.TestCase):
         expected_py = 'if (x == True):\n\tif (y == True):\n\t\tprint("y and x are true")'
         self.assertEqual(expected_py, gtc.gast_to_code(input_gast, "py"))
 
- 
+    def test_multi_for_of(self):
+        input_gast = {'type': 'root', 'body': [{'type': 'forOfStatement', 'init': {'type': 'name', 'value': 'j'}, 'iter': {'type': 'arr', 'elements': [{'type': 'num', 'value': 1}, {'type': 'num', 'value': 2}]}, 'body': [{'type': 'forOfStatement', 'init': {'type': 'name', 'value': 'k'}, 'iter': {'type': 'arr', 'elements': [{'type': 'num', 'value': 3}, {'type': 'num', 'value': 4}]}, 'body': [{'type': 'name', 'value': 'j'}, {'type': 'name', 'value': 'k'}]}]}]}
+
+        expected_js = 'for (j of [1, 2]) {\n\tfor (k of [3, 4]) {\n\t\tj\n\t\tk\n\t}\n}'
+        self.assertEqual(expected_js, gtc.gast_to_code(input_gast, "js"))
+
+        expected_py = 'for j in [1, 2]:\n\tfor k in [3, 4]:\n\t\tj\n\t\tk'
+        self.assertEqual(expected_py, gtc.gast_to_code(input_gast, "py"))
 
 if __name__ == '__main__':
     unittest2.main()
