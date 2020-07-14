883f343e09c378059fda343051dbebed54c16da7
Jack <jackdavidweber@gmail.com>
2020-07-13 17:36:49 -0700

    pulled languages in from converter_registry. added init to app.py where bootstrappingandpulling of languages is done

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
 from main import main
 from bootstrap import bootstrap
+from shared.gast_to_code.converter_registry import ConverterRegistry
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,14 +15,14 @@ parser.add_argument('in_lang')
 parser.add_argument('out_lang')
 
 class Translate(Resource):
+    def __init__(self):
+        bootstrap()
+        self.languages = ConverterRegistry.get_languages()
 
     def get(self):
-        bootstrap() # TODO: run bootstrap at loading of translate rather than on get request
-        return {'work': 'ing'}
+        return {'supported_languages': self.languages}
 
     def post(self):
-        languages = ["py", "js"] 
-
         # bring in post arguments
         args = parser.parse_args()
         input_code = args['input']
@@ -29,14 +30,20 @@ class Translate(Resource):
         output_lang = args['out_lang']
 
         output_code = main(input_code, input_lang, output_lang)
+        response_input_lang = input_lang
 
+        # automatic language detection
         i = 0
-        while (output_code == "Error: did not compile") and (i < len(languages)):
-            input_lang = languages[i]
-            output_code = main(input_code, input_lang, output_lang)
+        while (output_code == "Error: did not compile") and (i < len(self.languages)):
+            response_input_lang = self.languages[i]
+            output_code = main(input_code, response_input_lang, output_lang)
             i += 1
 
-        return {'response': output_code, 'response_in_lang': input_lang}
+        # ensures that response_in_lang is the same as requested in_lang if no languages compile
+        if output_code == "Error: did not compile":
+            response_input_lang = input_lang
+
+        return {'response': output_code, 'response_in_lang': response_input_lang}
 
 api.add_resource(Translate, '/')
 
