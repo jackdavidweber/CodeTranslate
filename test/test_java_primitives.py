a05aefc237a6f73799613b06ae94e18b6ff3583c
Steph Walsh <srwalsh21@gmail.com>
2020-07-15 10:46:02 -0400

    Add class and function wrapper to user code

--- test/test_java_primitives.py
+++ test/test_java_primitives.py
@@ -3,23 +3,10 @@ import main
 
 class TestJavaPrim(unittest2.TestCase):
     def test_basic(self):
-        code = '''
-        public class HelloWorld{
-            public static void main(String []args){
-                System.out.println();
-            }
-        }
-        '''
+        code = "System.out.println();"
         self.assertEqual("System.out.println()", main.main(code, 'java', 'java'))
     def test_basic_args(self):
-        code = '''
-        public class HelloWorld{
-            public static void main(String []args){
-                System.out.println("s");
-                1;
-            }
-        }
-        '''
+        code = 'System.out.println("s");\n 1;'
         self.assertEqual('System.out.println("s")\n1', main.main(code, 'java', 'java'))
 
 if __name__ == '__main__':
