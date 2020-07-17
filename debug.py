d28fd793252fa917477fbc37db46e689a3d843eb
Cory O'Shea <cjoshea9@gmail.com>
2020-07-17 11:08:01 -0700

    Checkout debug.py to revert changes

--- debug.py
+++ debug.py
@@ -1,3 +1,3 @@
-import java.code_to_gast.java_main as java_main
+import bash.code_to_gast.bash_main as bash_main
 
-print(java_main.java_to_gast('if (1) {System.out.println("1 is true");} else if (2) {System.out.println("2 is true"); System.out.println("second line");}'))
+print(bash_main.bash_to_gast('true'))
