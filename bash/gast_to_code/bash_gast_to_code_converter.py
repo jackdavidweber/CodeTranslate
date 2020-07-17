ee08bc1c6c597ed57a60a4c250998af3d641076c
Jack <jackdavidweber@gmail.com>
2020-07-16 19:02:20 -0700

    throw error if try to nest arrays in arrays

--- bash/gast_to_code/bash_gast_to_code_converter.py
+++ bash/gast_to_code/bash_gast_to_code_converter.py
@@ -19,9 +19,13 @@ class BashGastToCodeConverter(AbstractGastToCodeConverter):
         # bash has no way to directly print arrays. This logic returns an error for such behavior
         # TODO: maybe we should run code after translation to make sure it compiles and throw errors if it does NOT
         if gast["value"]["type"] == "logStatement" and general_helpers.arr_in_list(gast["args"]):
-            return "Error: direct translation not possible" # TODO: streamline error message as part of refactor
+            return "impossibleTranslationError: direct translation does not exist" # TODO: streamline error message as part of refactor
 
         return router.gast_to_code(gast["value"], "bash") + " " + router.gast_to_code(gast["args"], "bash")
 
     def handle_arr(gast):
+        # This logic returns an error for nested arrays which are not supported in bash
+        if general_helpers.arr_in_list(gast["elements"]):
+            return "impossibleTranslationError: direct translation does not exist" # TODO: streamline error message as part of refactor
+
         return "(" + router.gast_to_code(gast["elements"], "py") + ")"
