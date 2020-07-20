c63086c0114fac1ff8f2134a3a1a2538bdd60413
Jack <jackdavidweber@gmail.com>
2020-07-20 13:13:27 -0700

    refactored main function into helpers and added series of checks for errors

--- main.py
+++ main.py
@@ -15,41 +15,77 @@ output_lang: string representing output language of code
 return: string representing output code or error message
 """
 def main(input_code, input_lang, output_lang):
+    try:
+        # check arguments TODO(taiga#172): remove hard coded references
+        check_valid_args(input_code, input_lang, output_lang, ["js", "py", "java"], ["js", "py", "bash", "java"])
 
-    if input_lang == None and output_lang == None:
-        abort(400, "Error: must specify input and output languages")
+        # code to gast
+        gast = main_code_to_gast(input_code, input_lang)
+        check_valid_gast(gast)
 
-    if input_lang == "js":
-        gast = js_main.js_to_gast(input_code)
-    elif input_lang == "py":
-        gast = py_main.py_to_gast(input_code)
-    elif input_lang == "java":
-        gast = java_main.java_to_gast(input_code)
-    else:
-        #TODO: figure out hwo to do error messages
-        return "Error must specify input language. For example, js for javascript and py for python"
+        #gast to code
+        output_code = main_gast_to_code(gast, output_lang)
 
-    output_langs = ["js", "py", "bash", "java"]
-    if output_lang not in output_langs:
-        # TODO: send 400 client error
-        return "Error must specify output language. For example, js for javascript and py for python"
+        # analytics
+        main_analytics(input_code, output_code, input_lang, output_lang)
+
+        return output_code
+    except:
+        # This error should never occur but probably a good thing to have in case
+        return "Error: unable to execute main function"
+
+
+def main_code_to_gast(input_code, input_lang):
+    try:
+        # TODO(taiga#172): remove hard coded references
+        if input_lang == "js":
+            gast = js_main.js_to_gast(input_code)
+        elif input_lang == "py":
+            gast = py_main.py_to_gast(input_code)
+        elif input_lang == "java":
+            gast = java_main.java_to_gast(input_code)
+        else:
+            return "Error must specify input language. For example, js for javascript and py for python"
+
+        return gast
+    except:
+        return "Error: unable to convert code to generic ast"
+
+
+def check_valid_args(input_code, input_lang, output_lang, valid_input_langs, valid_output_langs):
+    if not (type(input_code) == str and type(input_lang) == str and type(output_lang) == str):
+        abort(400, "Error: invalid argument types")
+
+    if input_lang not in valid_input_langs:
+        abort(400, "Error must specify valid input language. For example, js for javascript and py for python")
    
-    output_code = ""
-    if (type(gast) == str) :
-        # return error if gast not built - dont store in database
-        output_code = "Error: did not compile"
-    else:
-        output_code = gtc.gast_to_code(gast, output_lang)
+    if output_lang not in valid_output_langs:
+        abort(400, "Error must specify valid output language. For example, js for javascript and py for python")
+   
 
+def check_valid_gast(gast):
+    if (type(gast) == str) : # FIXME(swalsh15) why is this necessary??
+        # return error if gast not built - dont store in database
+        abort(400, "Error: did not compile")
+
+
+def main_gast_to_code(gast, output_lang):
+    try:
+        output_code = gtc.gast_to_code(gast, output_lang)
+        
         if output_lang == "java":
             output_code = general_helpers.java_linter(output_code)
-    
+
+        return output_code
+    except:
+        return "Error: unable to convert generic ast to code"
+
+
+def main_analytics(input_code, output_code, input_lang, output_lang):
     # if the user deletes their translation don't store empty translation "" -> ""
     if (input_code == "" and output_code == ""):
-        return output_code
-    
-    # store translation on firebase
-    data_service = DataService.getInstance()
-    data_service.store_query(input_code, output_code, input_lang, output_lang)
-
-    return output_code
+        pass
+    else:
+        # store translation on firebase
+        data_service = DataService.getInstance()
+        data_service.store_query(input_code, output_code, input_lang, output_lang)
