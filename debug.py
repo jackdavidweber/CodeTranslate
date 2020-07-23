4024f0a216c594f9f75464b9fcc4295a8f546996
Steph Walsh <srwalsh21@gmail.com>
2020-07-23 10:50:33 -0400

    Trying to update test code

--- debug.py
+++ debug.py
@@ -4,9 +4,8 @@ from bootstrap import bootstrap
 # Register all languages before testing can begin
 bootstrap()
 
-print("****with functions****\n")
 print(
     main.main("function s() {2}\nconsole.log(1)\n2\n3\n4\nfunction t() {5}",
               "js", "java"))
-print("\n****with NO functions****\n")
 print(main.main("console.log(1)\n2\n3\n4", "js", "java"))
+
